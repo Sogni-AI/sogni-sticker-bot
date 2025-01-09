70dea58f3da5a1a12bb8a0feb260d2a611f0cb18
Mauvis Ledford <switchstatement@gmail.com>
2025-01-09 16:45:21 +0800

    restart script every 8 hours to avoid token auth issue.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    restart_delay: 3000
+    restart_delay: 3000,
+    cron_restart: "0 */8 * * *" // Restart every 8 hours
   }]
 }
